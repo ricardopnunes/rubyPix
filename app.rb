0f48ad91ecb8183cc0c7721e023df3c6850dd1ad
ricardopnunes <ricardopontin145@gmail.com>
2023-10-02 00:07:37 -0300

    adicionando rota de create users

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'dotenv'
+require './app/adapters/controllers/user_controllers'
 Dotenv.load
 
 set :database, { adapter: 'sqlite3', database: ENV['DB_PATH'] }
+
+post '/users' do
+  { result: UserController.create(params) }.to_json
+end
